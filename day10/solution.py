c5783d7675ef056b8c038663bb7c1fb35f1e1a50
BLannoo <brunolannoo@gmail.com>
2019-12-11 21:16:36 +0100

    solution day 10 part 2

--- day10/solution.py
+++ day10/solution.py
@@ -1,5 +1,9 @@
+from math import sqrt, pi
 from typing import Tuple, Set
 from unittest import TestCase
+
+import pandas as pd
+from numpy.ma import arccos
 
 
 def generate(pattern) -> Set[Tuple[int, int]]:
@@ -118,4 +122,97 @@ class TestSilver(TestCase):
         self.assertEqual(
             ((20, 21), 247),
             best_location(field)
+        )
+
+
+def dist(p1, p2):
+    return sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+
+
+# cosine rule: https://stackoverflow.com/questions/1211212/how-to-calculate-an-angle-from-three-points
+def angle_from_up(target, corner=(0, 0)):
+    reference = (corner[0], corner[1] - 1)
+    p1 = corner
+    p2 = reference
+    p3 = target
+    p12 = dist(p1, p2)
+    p13 = dist(p1, p3)
+    p23 = dist(p2, p3)
+    radials = arccos((p12 ** 2 + p13 ** 2 - p23 ** 2) / (2 * p12 * p13))
+    angle = radials * 180 / pi
+    if target[0] < reference[0]:
+        angle = 360 - angle
+    return angle
+
+
+def solve_gold(field, station):
+    field.remove(station)
+    df = pd.DataFrame(field, columns=['x', 'y'])
+    df['angle'] = df.apply(
+        lambda row: round(
+            angle_from_up((row.x, row.y), corner=station)
+            , 5
+        ),
+        axis=1
+    )
+    df['distance'] = df.apply(
+        lambda row: round(dist(station, (row.x, row.y)), 5),
+        axis=1
+    )
+    df.sort_values(['angle', 'distance'], inplace=True)
+    asteroid_200 = df.groupby(by='angle').first().iloc[199]
+    return asteroid_200
+
+
+class TestGold(TestCase):
+    def test_example_a(self):
+        self.assertEqual(angle_from_up((0, -1)), 0)
+        self.assertAlmostEqual(angle_from_up((1, -1)), 45)
+        self.assertAlmostEqual(angle_from_up((1, 0)), 90)
+        self.assertAlmostEqual(angle_from_up((1, 1)), 135)
+        self.assertAlmostEqual(angle_from_up((0, 1)), 180)
+        self.assertAlmostEqual(angle_from_up((-1, 1)), 225)
+        self.assertAlmostEqual(angle_from_up((-1, 0)), 270)
+        self.assertAlmostEqual(angle_from_up((-1, -1)), 315)
+        self.assertAlmostEqual(angle_from_up((0, -1)), 0)
+
+    def test_example_0(self):
+        field = generate('''
+            .#..##.###...#######
+            ##.############..##.
+            .#.######.########.#
+            .###.#######.####.#.
+            #####.##.#.##.###.##
+            ..#####..#.#########
+            ####################
+            #.####....###.#.#.##
+            ##.#################
+            #####.##.###..####..
+            ..######..##.#######
+            ####.##.####...##..#
+            .#####..#.######.###
+            ##...#.##########...
+            #.##########.#######
+            .####.#.###.###.#.##
+            ....##.##.###..#####
+            .#.#.###########.###
+            #.#.#.#####.####.###
+            ###.##.####.##.#..##
+        '''.replace(' ', ''))
+
+        asteroid_200 = solve_gold(field, station=(11, 13))
+        self.assertEqual(
+            (asteroid_200.x, asteroid_200.y),
+            (8, 2)
+        )
+
+    def test_assignement(self):
+        with open('data.txt') as file:
+            data = ''.join(file.readlines())
+        field = generate(data)
+
+        asteroid_200 = solve_gold(field, station=(20, 21))
+        self.assertEqual(
+            (asteroid_200.x, asteroid_200.y),
+            (19, 19)
         )
