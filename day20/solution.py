b2c6a77194e2426a4a48794b26c8869854951c16
BLannoo <brunolannoo@gmail.com>
2019-12-22 17:50:17 +0100

    day 20 part 2

--- day20/solution.py
+++ day20/solution.py
@@ -1,4 +1,5 @@
-from typing import List, Tuple, Set
+from pprint import pprint
+from typing import List, Tuple, Set, Dict
 from unittest import TestCase
 
 import numpy as np
@@ -13,19 +14,44 @@ def read_labyrinth(file_name: str):
     return map
 
 
-def print_map(map, highlight: List[Tuple[int, int]] = []):
-    YELLOW = '\033[93m'
-    WHITE = '\033[0m'
+def print_map(map, highlight: List[Tuple[int, int, int]] = [(0, 0, 0)]):
+    end_color = '\033[0;0m'
+    colors = ['\033[0;{}m'.format(code) for code in range(100, 108)]
+
+    min_depth = min(depth for _, _, depth in highlight)
+    max_depth = max(depth for _, _, depth in highlight)
+
+    for depth in range(min_depth, max_depth + 1):
+        print(colors[depth % len(colors)] + str(depth) + end_color, end='')
+    print()
+
     for i in range(map.shape[0]):
         for j in range(map.shape[1]):
-            if (i, j) in highlight:
-                print(YELLOW + map[i, j] + WHITE, end='')
-            else:
-                print(map[i, j], end='')
+            cell = map[i, j]
+            for depth in range(min_depth, max_depth + 1):
+                if (i, j, depth) in highlight:
+                    cell = colors[depth % len(colors)] + map[i, j] + end_color
+            print(cell, end='')
         print()
 
+    print(highlight)
 
-def find_portals(name: str, map) -> Set[Tuple[int, int]]:
+
+def with_depth(x: int, y: int, map) -> Tuple[int, int, int]:
+    (width, height) = map.shape
+    if (
+            x == 3
+            or
+            x == width - 4
+            or
+            y == 3
+            or y == height - 4
+    ):
+        return x, y, -1
+    return x, y, 1
+
+
+def find_portals(name: str, map) -> Set[Tuple[int, int, int]]:
     entrances = set()
     first_letter_options = tuple(zip(*np.nonzero(map == name[0])))
     for (x_1, y_1) in first_letter_options:
@@ -38,9 +64,9 @@ def find_portals(name: str, map) -> Set[Tuple[int, int]]:
                 x_back = x_1 - x_dir
                 y_back = y_1 - y_dir
                 if map[x_further, y_further] == '.':
-                    entrances.add((x_further, y_further))
+                    entrances.add(with_depth(x_further, y_further, map))
                 elif map[x_back, y_back] == '.':
-                    entrances.add((x_back, y_back))
+                    entrances.add(with_depth(x_back, y_back, map))
                 else:
                     raise Exception("portal '{}' not next to entrance".format(name))
     return entrances
@@ -49,8 +75,8 @@ def find_portals(name: str, map) -> Set[Tuple[int, int]]:
 ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 
-def read_portal(location: Tuple[int, int], map) -> str:
-    original_letter = map[location]
+def read_portal(location: Tuple[int, int, int], map) -> str:
+    original_letter = map[location[0], location[1]]
 
     second_letter = map[location[0] + 1, location[1]]
     if second_letter in ALPHABET:
@@ -70,30 +96,37 @@ def read_portal(location: Tuple[int, int], map) -> str:
 DIRECTIONS = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
 
-def movement_options(location: Tuple[int, int], map) -> Set[Tuple[int, int]]:
+def movement_options(location: Tuple[int, int, int], map) -> Set[Tuple[int, int, int]]:
     options = set()
     for dx, dy in DIRECTIONS:
-        option = location[0] + dx, location[1] + dy
-        if map[option] == '.':
+        option = location[0] + dx, location[1] + dy, location[2]
+        if map[option[0], option[1]] == '.':
             options.add(option)
-        elif map[option] in ALPHABET:
+        elif map[option[0], option[1]] in ALPHABET:
             portals = find_portals(read_portal(option, map), map)
-            portals.remove(location)
-            if len(portals) == 1:
-                options.add(portals.pop())
+            for portal in portals:
+                if portal[0] != location[0] or portal[1] != location[1]:
+                    options.add((portal[0], portal[1], location[2] + portal[2]))
     return options
 
 
-def is_finish(location: Tuple[int, int], map) -> bool:
+def is_finish(location: Tuple[int, int, int], map, consider_depth=False) -> bool:
     for dx, dy in DIRECTIONS:
-        if read_portal((location[0] + dx, location[1] + dy), map) == 'ZZ':
-            return True
+        if read_portal((location[0] + dx, location[1] + dy, location[2]), map) == 'ZZ':
+            if consider_depth:
+                return location[2] == 0
+            else:
+                return True
     return False
 
 
-def breadth_first(start_location, map):
+def breadth_first(
+        start_location: Tuple[int, int, int],
+        map,
+        consider_depth: bool = False
+) -> List[List[Tuple[int, int, int]]]:
     paths = [[start_location]]
-    while not is_finish(paths[0][-1], map):
+    while not is_finish(paths[0][-1], map, consider_depth):
         path = paths[0]
         options = movement_options(path[-1], map)
         for option in options:
@@ -105,24 +138,55 @@ def breadth_first(start_location, map):
     return paths
 
 
-def solve_silver(temp):
-    map = read_labyrinth(temp)
+def solve_silver(filename):
+    map = read_labyrinth(filename)
     start_location = find_portals('AA', map).pop()
-    paths = breadth_first(start_location, map)
+    paths = breadth_first((start_location[0], start_location[1], 0), map)
     print_map(map, highlight=paths[0])
-    silver = len(paths[0]) - 1
-    return silver
+    return len(paths[0]) - 1
+
+
+def simplify(map):
+    map = map.copy()
+    simplified_in_last_iteration = True
+    while simplified_in_last_iteration:
+        simplified_in_last_iteration = False
+        for i in range(map.shape[0]):
+            for j in range(map.shape[1]):
+                # if . surrounded by 3 #
+                if (
+                        map[i, j] == '.'
+                        and
+                        len([
+                            map[i + direction[0], j + direction[1]]
+                            for direction in DIRECTIONS
+                            if map[i + direction[0], j + direction[1]] == '#'
+                        ]) == 3
+                ):
+                    map[i, j] = '#'
+                    simplified_in_last_iteration = True
+    return map
+
+
+def solve_gold(filename: str) -> int:
+    map = read_labyrinth(filename)
+    simplified_map = simplify(map)
+    print_map(simplified_map)
+    start_location = find_portals('AA', simplified_map).pop()
+    paths = breadth_first((start_location[0], start_location[1], 0), simplified_map, consider_depth=True)
+    print_map(map, highlight=paths[0])
+    return len(paths[0]) - 1
 
 
 class TestSilver(TestCase):
     def test_find_portal(self):
         map = read_labyrinth('example0.txt')
         self.assertSetEqual(
-            {(3, 10)},
+            {(3, 10, -1)},
             find_portals('AA', map)
         )
         self.assertSetEqual(
-            {(7, 10), (9, 3)},
+            {(7, 10, 1), (9, 3, -1)},
             find_portals('BC', map)
         )
 
@@ -130,47 +194,57 @@ class TestSilver(TestCase):
         map = read_labyrinth('example0.txt')
         self.assertEqual(
             'AA',
-            read_portal((2, 10), map)
+            read_portal((2, 10, 0), map)
         )
         self.assertEqual(
             'BC',
-            read_portal((8, 10), map)
+            read_portal((8, 10, 0), map)
         )
         self.assertEqual(
             'BC',
-            read_portal((9, 2), map)
+            read_portal((9, 2, 0), map)
         )
         self.assertEqual(
             'ZZ',
-            read_portal((18, 14), map)
+            read_portal((18, 14, 0), map)
         )
 
     def test_movement_options(self):
         map = read_labyrinth('example0.txt')
         self.assertSetEqual(
-            {(4, 10)},
-            movement_options((3, 10), map)
+            {(4, 10, 0)},
+            movement_options((3, 10, 0), map)
         )
         self.assertSetEqual(
-            {(3, 10), (5, 10), (4, 11)},
-            movement_options((4, 10), map)
+            {(3, 10, 0), (5, 10, 0), (4, 11, 0)},
+            movement_options((4, 10, 0), map)
         )
         self.assertSetEqual(
-            {(6, 10), (9, 3)},
-            movement_options((7, 10), map)
+            {(6, 10, 0), (9, 3, -1)},
+            movement_options((7, 10, 0), map)
+        )
+        self.assertSetEqual(
+            {(9, 4, 0), (7, 10, 1)},
+            movement_options((9, 3, 0), map)
         )
 
     def test_is_finish(self):
         map = read_labyrinth('example0.txt')
 
         self.assertTrue(
-            is_finish((17, 14), map)
+            is_finish((17, 14, 0), map)
+        )
+        self.assertTrue(
+            is_finish((17, 14, 1), map)
+        )
+        self.assertTrue(
+            is_finish((17, 14, -1), map)
         )
         self.assertFalse(
-            is_finish((3, 10), map)
+            is_finish((3, 10, 0), map)
         )
         self.assertFalse(
-            is_finish((4, 10), map)
+            is_finish((4, 10, 0), map)
         )
 
     def test_example0(self):
@@ -189,4 +263,257 @@ class TestSilver(TestCase):
         self.assertEqual(
             570,
             solve_silver('data.txt')
+        )
+
+
+class Segment:
+    def __init__(
+            self,
+            start: Tuple[int, int],
+            end: Tuple[int, int],
+            length: int,
+            dz: int,
+            path: List[Tuple[int, int, int]] = []
+    ):
+        self.start = start
+        self.end = end
+        self.length = length
+        self.dz = dz
+        self.path = path.copy()
+
+    def contains(self, location: Tuple[int, int]) -> bool:
+        return location in {
+            (x, y)
+            for x, y, z in self.path
+        }
+
+    def __eq__(self, other: 'Segment') -> bool:
+        return (
+                self.start == other.start
+                and
+                self.end == other.end
+                and
+                self.length == other.length
+                and
+                self.dz == other.dz
+        )
+
+    def __hash__(self) -> int:
+        return hash((self.start, self.end))
+
+    def __repr__(self) -> str:
+        return 'Segment(start={}, end={}, length={}, dz={})'.format(
+            self.start, self.end, self.length, self.dz
+        )
+
+
+class Path:
+    def __init__(self, seed: Tuple[int, int]):
+        self.segments: List[Segment] = [Segment(start=seed, end=seed, length=0, dz=0)]
+
+    def start(self) -> Tuple[int, int]:
+        return self.segments[0].start
+
+    def end(self) -> Tuple[int, int]:
+        return self.segments[-1].end
+
+    def length(self) -> int:
+        return sum([segment.length for segment in self.segments])
+
+    def dz(self) -> int:
+        return sum([segment.dz for segment in self.segments])
+
+    def extend(self, segment: Segment) -> None:
+        self.segments.append(segment)
+
+    def fits(self, new_segment: Segment) -> bool:
+        return self.segments[-1].end == new_segment.start
+
+    def __repr__(self) -> str:
+        return 'Path with: start={}, end={}, lenght={} and dz={}'.format(
+            self.start(), self.end(), self.length(), self.dz
+        )
+
+
+def solve_gold_2(filename: str) -> Set[Segment]:
+    map = read_labyrinth(filename)
+    simplified_map = simplify(map)
+    real_branchings = find_real_branchings(simplified_map)
+    location_AA = find_portals('AA', simplified_map).pop()
+    location_ZZ = find_portals('ZZ', simplified_map).pop()
+    vertices = {
+        (location_AA[0], location_AA[1]),
+        *real_branchings,
+        (location_ZZ[0], location_ZZ[1])
+    }
+    print_map(simplified_map, [(x, y, 0) for x, y in vertices])
+    segments = generate_segments(vertices, simplified_map)
+
+    pprint(segments)
+
+    print_map_with_segments(map, segments)
+
+    return segments
+
+
+def find_all_portals(simplified_map) -> Dict[str, Set[Tuple[int, int, int]]]:
+    portals = {}
+    for char_1 in ALPHABET:
+        for char_2 in ALPHABET:
+            name = char_1 + char_2
+            found = find_portals(name, simplified_map)
+            if len(found) != 0:
+                portals[name] = found
+    return portals
+
+
+def find_real_branchings(map) -> Set[Tuple[int, int]]:
+    map = simplify(map)
+    branches = set()
+    for i in range(map.shape[0]):
+        for j in range(map.shape[1]):
+            if (
+                    map[i, j] == '.'
+                    and
+                    len([
+                        map[i + direction[0], j + direction[1]]
+                        for direction in DIRECTIONS
+                        if map[i + direction[0], j + direction[1]] == '.'
+                    ]) >= 3
+            ):
+                branches.add((i, j))
+    return branches
+
+
+def generate_segments(vertices: Set[Tuple[int, int]], map) -> Set[Segment]:
+    all_segments = []
+    for (x, y) in vertices:
+        start = (x, y, 0)
+        segments = [
+            [start, option]
+            for option in movement_options(start, map)
+        ]
+        for segment in segments:
+            while (segment[-1][0], segment[-1][1]) not in vertices:
+                options = movement_options(segment[-1], map)
+                if len(segment) >= 2:
+                    options.remove(segment[-2])
+                segment.append(options.pop())
+        all_segments.extend(segments)
+    return {
+        Segment(
+            start=(path[0][0], path[0][1]),
+            end=(path[-1][0], path[-1][1]),
+            length=len(path) - 1,
+            dz=path[-1][2] - path[0][2],
+            path=path
+        )
+        for path in all_segments
+
+        # eliminates duplicates ( start < end )
+        if (path[0][0], path[0][1]) < (path[-1][0], path[-1][1])
+    }
+
+
+def print_map_with_segments(map, highlight: Set[Segment]):
+    end_color = '\033[0;0m'
+    colors = ['\033[0;{}m'.format(code) for code in range(100, 108)]
+
+    for i, segment in enumerate(highlight):
+        print(colors[i % len(colors)] + str(segment) + end_color)
+
+    for i in range(map.shape[0]):
+        for j in range(map.shape[1]):
+            cell = map[i, j]
+            for index, segment in enumerate(highlight):
+                if segment.contains((i, j)):
+                    cell = colors[index % len(colors)] + map[i, j] + end_color
+            print(cell, end='')
+        print()
+
+    print(highlight)
+
+
+class TestGold(TestCase):
+    def test_example0(self):
+        self.assertEqual(
+            26,
+            solve_gold('example0.txt')
+        )
+
+    def test_example2(self):
+        self.assertEqual(
+            228,  # assignement say 396
+            solve_gold('example2.txt')
+        )
+
+    def test_find_all_portals(self):
+        map = read_labyrinth('example0.txt')
+        self.assertDictEqual(
+            find_all_portals(map),
+            {
+                'AA': {(3, 10, -1)},
+                'BC': {(9, 3, -1), (7, 10, 1)},
+                'DE': {(14, 3, -1), (11, 7, 1)},
+                'FG': {(16, 3, -1), (13, 12, 1)},
+                'ZZ': {(17, 14, -1)}
+            }
+        )
+
+    def test_find_real_branchings(self):
+        map = read_labyrinth('example0.txt')
+        self.assertSetEqual(
+            find_real_branchings(map),
+            {(4, 10), (16, 14)}
+        )
+
+    def test_generate_segments(self):
+        map = read_labyrinth('example0.txt')
+        map = simplify(map)
+        segments = generate_segments({(3, 10), (4, 10), (16, 14), (17, 14)}, map)
+        self.assertSetEqual(
+            {
+                Segment(start=(4, 10), end=(16, 14), length=24, dz=0),
+                Segment(start=(3, 10), end=(4, 10), length=1, dz=0),
+                Segment(start=(4, 10), end=(16, 14), length=21, dz=-1),
+                Segment(start=(16, 14), end=(17, 14), length=1, dz=0),
+            },
+            segments
+        )
+
+    def test_example2_2(self):
+        solve_gold_2('example2.txt')
+
+    # 1136 is too low
+    # 5434 is too low
+    # 7264 is too high
+    # answer is 7056
+    # based on assembling segments manually
+    # solution is:
+    # A + (-D) which is the only initial path: 150 steps, -3 floors
+    # B + F + (-G) which is a loop of: 1157 steps, -19 floors
+    # 2 times: D + (-C) + F + (-G) + D + (-C) + (-B) which is a double loop of: 1537+484 steps, -11+8 floors
+    # 2 times: D + (-C) + (-B) which is a loop of: 484 steps, +8 floors
+    # G + (-E) which is the only final path: 739 steps, +12 floors
+    def test_assignement(self):
+        segments = {
+            'A': Segment(start=(62, 3), end=(62, 4), length=1, dz=0),
+            'B': Segment(start=(4, 72), end=(32, 72), length=52, dz=0),
+            'C': Segment(start=(32, 72), end=(62, 4), length=283, dz=-5),
+            'D': Segment(start=(4, 72), end=(62, 4), length=149, dz=3),
+            'E': Segment(start=(86, 3), end=(86, 4), length=1, dz=0),
+            'F': Segment(start=(32, 72), end=(86, 4), length=367, dz=-7),
+            'G': Segment(start=(4, 72), end=(86, 4), length=738, dz=12),
+        }
+        self.assertSetEqual(
+            {segment for segment in segments.values()},
+            solve_gold_2('data.txt')
+        )
+        self.assertEqual(
+            0,
+            -3-19+2*(-11+8)+2*8+12
+        )
+        self.assertEqual(
+            7056,
+            150+1157+2*(1537+484)+2*484+739
         )
