fa7a7ad545cb6de2a2241fed92df1f4acecc28d3
BLannoo <brunolannoo@gmail.com>
2019-12-23 17:50:29 +0100

    day 23 part 2

--- day23/solution.py
+++ day23/solution.py
@@ -12,6 +12,8 @@ class Network:
             for i in range(num_computers)
         ]
         self.packets: Dict[int, List[List[int]]] = defaultdict(lambda: [])
+        self.rounds_idle = 0
+        self.nat_delivered_packets = []
 
     def pop_packet(self, address: int) -> List[int]:
         packets = self.packets[address]
@@ -21,19 +23,36 @@ class Network:
             return [-1]
 
     def tick(self):
-        packets = [
-            self.pop_packet(address)
-            for address in range(len(self.computers))
-        ]
-        for current_address, computer in enumerate(self.computers):
-            print(f'computer {current_address:2} receiving: {packets[current_address]}')
-            computer.add_input(packets[current_address])
-            computer.run_program()
-            for i in range(int(len(computer.output) / 3)):
-                destination, x, y = computer.output[i * 3:i * 3 + 3]
+        packets = self.pop_packets()
+        for current_address in range(len(self.computers)):
+            self.computers[current_address].add_input(packets[current_address])
+
+        for current_address in range(len(self.computers)):
+            self.computers[current_address].run_program()
+
+        had_output = False
+        for current_address in range(len(self.computers)):
+            for i in range(int(len(self.computers[current_address].output) / 3)):
+                had_output = True
+                destination, x, y = self.computers[current_address].output[i * 3:i * 3 + 3]
                 self.packets[destination].append([x, y])
-                print(f'computer {current_address:2} sending: {computer.output[i * 3:i * 3 + 3]}')
-            computer.output = []
+                if destination >= len(self.computers):
+                    print(
+                        f'computer {current_address:2} sending [{x}, {y}] to {destination}')
+            self.computers[current_address].output = []
+
+        # if was idle
+        if any([packet != [-1] for packet in packets]) == 0 and not had_output:
+            if self.rounds_idle >= 5:
+                nat_packet = self.packets[255][-1]
+                self.packets[0].append(nat_packet)
+                self.nat_delivered_packets.append(nat_packet)
+                self.rounds_idle = 0
+            else:
+                self.rounds_idle += 1
+
+    def pop_packets(self):
+        return [self.pop_packet(address) for address in range(len(self.computers))]
 
 
 def solve_silver() -> List[List[int]]:
@@ -44,9 +63,43 @@ def solve_silver() -> List[List[int]]:
     return network.packets[255]
 
 
+def solve_gold() -> List[int]:
+    data = read_data()
+    network = Network(num_computers=50, program=data)
+    # for _ in range(1000):
+    while len(network.nat_delivered_packets) < 2 or (network.nat_delivered_packets[-1] != network.nat_delivered_packets[-2]):
+        network.tick()
+
+    print([
+        y
+        for x, y in network.packets[255]
+    ])
+    print([
+        y
+        for x, y in network.nat_delivered_packets
+    ])
+    return network.nat_delivered_packets[-1]
+
+
 class TestSilver(TestCase):
     def test_assignement(self):
         self.assertListEqual(
             [[93889, 22650]],
             solve_silver()
+        )
+
+
+class TestGold(TestCase):
+    # not twice in a row:
+    # 17363 is too high
+    #
+    # not delivered, but stored:
+    # 17300 is too high
+    #
+    # 17200 is too low (random guess)
+    # 17292 is wrong (random guess)
+    def test_assignement(self):
+        self.assertListEqual(
+            [93889, 17298],
+            solve_gold()
         )
